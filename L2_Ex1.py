cd166a847791f463ecc747294e2c5fe3ae55c302
SZoult <tienpmfs4522@gmail.com>
2026-02-05 13:46:37 +0700

    All Assignment

--- L2_Ex1.py
+++
@@ -1,4 +0,0 @@
-Hours=input("Enter Hours: ")
-SalaryPerHour=input("Enter Salary per Hour: ")
-Salary=float(Hours)*float(SalaryPerHour)    
-print("Salary: ",Salary)
--- L3_Ex1.py
+++
@@ -1,6 +0,0 @@
-Hours = int(input("Enter Hours: "))
-Rate = int(input("Enter Rate: "))
-if Hours > 40:
-    print("Pay: ", (40 * Rate) + ((Hours - 40) * Rate * 1.5))
-else:
-    print("Pay: ", Hours * Rate)
